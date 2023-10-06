2aab9268c1cd881afb2e392a084a5e5385007d6c
gwadholm <gwadholm@gmail.com>
2023-10-06 13:37:36 -0400

    finished css styling. Added the gameMessage option to the logic. Changed the game messages to be templates that I write to the h3 element.

--- script.js
+++ script.js
@@ -4,14 +4,16 @@ const main = document.querySelector('main');
 let totalWins = 0;
 let totalTies = 0;
 let totalLosses = 0;
-let computerLetterChoice = ["r", "p", "s"];
+let computerLetterChoice = ["rock", "paper", "scissors"];
 
 const template= `
   <h1>Rock Paper Scissors</h1>
     <h2 id="winTally">Wins: <span class="total">${totalWins}</span></h2>
     <h2 id="tieTally">Ties: <span class="total">${totalTies}</span></h2>
     <h2 id="lossTally">Losses: <span class="total">${totalLosses}</span></h2>
+    <h3 id="gameMessage"></h3>
 `;
+
 
 main.innerHTML = template;
 
@@ -20,16 +22,22 @@ for(let i=0; i<buttons.length; i++) {
   buttons[i].addEventListener("click", function(e) {
     let userChoice = e.target.value;
     let computerChoice = computerLetterChoice[Math.round(Math.random() * (2))];
-  
-    if ((userChoice === "r" && computerChoice === "r") || (userChoice === "p" && computerChoice === "p") || (userChoice === "s" &&  computerChoice === "s")) {
+    let alertMessageTie = `It's a tie! You picked ${userChoice} and the computer picked ${computerChoice}.`
+    let alertMessageWin = `You won! You picked ${userChoice} and the computer picked ${computerChoice}.` 
+    let alertMessageLose = `Sorry, you lost. You picked ${userChoice} and the computer picked ${computerChoice}.`
+
+    if ((userChoice === "rock" && computerChoice === "rock") || (userChoice === "paper" && computerChoice === "paper") || (userChoice === "scissors" &&  computerChoice === "scissors")) {
       totalTies++;
       document.querySelector("#tieTally span").textContent = totalTies;
-    } else if ((userChoice === "r" && computerChoice === "s") || (userChoice === "p" && computerChoice === "r") || (userChoice === "s" &&  computerChoice === "p")) {
+      document.querySelector("h3#gameMessage").innerHTML = alertMessageTie;
+    } else if ((userChoice === "rock" && computerChoice === "scissors") || (userChoice === "paper" && computerChoice === "rock") || (userChoice === "scissors" &&  computerChoice === "paper")) {
       totalWins++;
       document.querySelector("#winTally span").textContent = totalWins;
-    } else if ((userChoice === "r" && computerChoice === "p") || (userChoice === "p" && computerChoice === "s") || (userChoice === "s" &&  computerChoice === "r")) {
+      document.querySelector("h3#gameMessage").innerHTML = alertMessageWin;
+    } else if ((userChoice === "rock" && computerChoice === "paper") || (userChoice === "paper" && computerChoice === "scissors") || (userChoice === "scissors" &&  computerChoice === "rock")) {
       totalLosses++;
       document.querySelector("#lossTally span").textContent = totalLosses;
+      document.querySelector("h3#gameMessage").innerHTML = alertMessageLose;
     }
    })
   }
