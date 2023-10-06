b38a63c0be77484a4640c2aa87ab58419f42b7f0
gwadholm <gwadholm@gmail.com>
2023-10-06 13:43:41 -0400

    cleaned up code

--- script.js
+++ script.js
@@ -14,30 +14,34 @@ const template= `
     <h3 id="gameMessage"></h3>
 `;
 
-
 main.innerHTML = template;
 
 for(let i=0; i<buttons.length; i++) {
-
   buttons[i].addEventListener("click", function(e) {
     let userChoice = e.target.value;
     let computerChoice = computerLetterChoice[Math.round(Math.random() * (2))];
-    let alertMessageTie = `It's a tie! You picked ${userChoice} and the computer picked ${computerChoice}.`
-    let alertMessageWin = `You won! You picked ${userChoice} and the computer picked ${computerChoice}.` 
-    let alertMessageLose = `Sorry, you lost. You picked ${userChoice} and the computer picked ${computerChoice}.`
+    let alertMessageTie = `It's a tie! You picked ${userChoice} and the computer picked ${computerChoice}.`;
+    let alertMessageWin = `You won! You picked ${userChoice} and the computer picked ${computerChoice}.`; 
+    let alertMessageLose = `Sorry, you lost. You picked ${userChoice} and the computer picked ${computerChoice}.`;
 
     if ((userChoice === "rock" && computerChoice === "rock") || (userChoice === "paper" && computerChoice === "paper") || (userChoice === "scissors" &&  computerChoice === "scissors")) {
+
       totalTies++;
       document.querySelector("#tieTally span").textContent = totalTies;
       document.querySelector("h3#gameMessage").innerHTML = alertMessageTie;
+
     } else if ((userChoice === "rock" && computerChoice === "scissors") || (userChoice === "paper" && computerChoice === "rock") || (userChoice === "scissors" &&  computerChoice === "paper")) {
+
       totalWins++;
       document.querySelector("#winTally span").textContent = totalWins;
       document.querySelector("h3#gameMessage").innerHTML = alertMessageWin;
+
     } else if ((userChoice === "rock" && computerChoice === "paper") || (userChoice === "paper" && computerChoice === "scissors") || (userChoice === "scissors" &&  computerChoice === "rock")) {
+
       totalLosses++;
       document.querySelector("#lossTally span").textContent = totalLosses;
       document.querySelector("h3#gameMessage").innerHTML = alertMessageLose;
+      
     }
-   })
-  }
+  })
+}
